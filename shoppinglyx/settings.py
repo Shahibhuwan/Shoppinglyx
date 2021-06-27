f7441b5f0c182566c307653b1cb4ecb548f0d911
Bhuwan Shahi <shahibhuwan5555@gmail.com>
2021-06-27 23:51:25 +0545

    allowed host are added  ie shoppinglyxecom.com

--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-n)f6i1d&-$%j5dk$n8&8ju9y%$n-9$zv5tn+d^!$p*h6pj&p8(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://shoppinglyxecom.herokuapp.com/','http://127.0.0.1:8000/','.shoppinlyxecom.com']
 
 
 # Application definition
