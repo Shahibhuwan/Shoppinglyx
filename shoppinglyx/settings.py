0291b3a8291cb8e40aac895159df2a2186467ebe
Bhuwan Shahi <shahibhuwan5555@gmail.com>
2021-06-27 23:51:53 +0545

    debug is false

--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n)f6i1d&-$%j5dk$n8&8ju9y%$n-9$zv5tn+d^!$p*h6pj&p8('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://shoppinglyxecom.herokuapp.com/','http://127.0.0.1:8000/','.shoppinlyxecom.com']
 
