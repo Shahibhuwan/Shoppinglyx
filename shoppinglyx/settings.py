f35284d972208a7a52273dc6f53efba554e4c3f3
Bhuwan Shahi <shahibhuwan5555@gmail.com>
2021-06-27 19:47:33 +0545

    whitenoise

--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'app',
 ]
 
-MIDDLEWARE = [ 'whitenoise.middleware.WhiteNoiseMiddleware',
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
